bc59fb5592a1bfdb81826ecbeccfd5600515ce48
dcpasha <66342395+dcpasha@users.noreply.github.com>
2021-04-08 16:01:03 -0400

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -1,7 +1,26 @@
 # Checking a list The word IN allows us to check if an item is in a list. The example shows the name and population for the countries:
 SELECT name, population FROM world
-WHERE name IN ('Brazil', 'Russia', 'India', 'China');
+  WHERE name IN ('Brazil', 'Russia', 'India', 'China');
 
 # BETWEEN allows range checking (range specified is inclusive of boundary values). 
 SELECT name, area FROM world
-WHERE area BETWEEN 200000 AND 250000
+  WHERE area BETWEEN 200000 AND 250000
+
+# Pattern Matching Queries
+# LIKE operator to check names. The % is a wild-card it can match any characters
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+# A name that has three letters a. ex: 'Bahama'
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+# '_%' You can use the underscore as a single character wildcard.
+# India and Angola have an n as the second character. 
+SELECT name FROM world
+ WHERE name LIKE '_n%'
+ORDER BY name
+
+# Find the countries that have two "o" characters separated by two others.
+SELECT name FROM world  
+ WHERE name LIKE '%o__o%'
