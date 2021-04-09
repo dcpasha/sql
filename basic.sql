394b37f52b6f1c54dd33cba7b34eab2813e26510
dcpasha <66342395+dcpasha@users.noreply.github.com>
2021-04-09 19:27:49 -0400

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -71,4 +71,49 @@ WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%'  AND name LIKE '%o
 
 
 --
+--
 -- Nested Statements
+
+
+--
+--Aggregate functions: COUNT, SUM and AVG
+--It takes many values and returns one.
+--What is the total population of ('Estonia', 'Latvia', 'Lithuania')?
+select sum(population)
+from world
+where name in  ('Estonia', 'Latvia', 'Lithuania')
+
+--HAVING and GROUPBY:
+--By including a GROUP BY clause functions such as SUM and COUNT are applied to groups of items sharing values. 
+--When you specify GROUP BY continent the result is that you get only one row for each different value of continent. 
+--All the other columns must be "aggregated" by one of SUM, COUNT ...
+
+--The HAVING clause allows use to filter the groups which are displayed. The WHERE clause filters rows before the aggregation, 
+--the HAVING clause filters after the aggregation.
+--If a ORDER BY clause is included we can refer to columns by their position.
+
+--For each continent show the number of countries:
+SELECT continent, COUNT(name)
+  FROM world
+ GROUP BY continent
+
+--WHERE and GROUP BY. The WHERE filter takes place before the aggregating function. 
+--For each relevant continent show the number of countries that has a population of at least 200000000.
+SELECT continent, COUNT(name)
+  FROM world
+ WHERE population>200000000
+ GROUP BY continent
+ 
+ --GROUP BY and HAVING. The HAVING clause is tested after the GROUP BY. 
+ --Show the total population of those continents with a total population of at least half a billion.
+SELECT continent, SUM(population)
+  FROM world
+ GROUP BY continent
+HAVING SUM(population)>500000000
+
+--For each continent show the continent and number of countries with populations of at least 10 million.
+select continent, count(name)
+from world
+where population > 10000000
+group by continent
+
